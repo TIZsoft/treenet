40db11ffa22c51bf6b50d0c0ac627299c7a5e475
Jian Zi-yao <ziyao@tizsoft.com>
2014-09-22 00:23:31 +0800

    Restructure solution.

---
+++ TIZSoft/Collections/Pool.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Tizsoft.Collections
+{
+    /// <summary>
+    /// Represents a generic and thread-safe pool of objects.
+    /// </summary>
+    /// <typeparam name="T">Type of item.</typeparam>
+    public class Pool<T> : IDisposable
+    {
+        readonly ConcurrentBag<T> _objects;
+        readonly Func<T> _objectGenerator;
+
+        bool _isDisposed;
+
+        public int Count
+        {
+            get { return _objects.Count; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return _objects.IsEmpty; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pool{T}"/> class.
+        /// </summary>
+        /// <param name="objectGenerator">A function of object generation.</param>
+        public Pool(Func<T> objectGenerator)
+        {
+            if (objectGenerator == null)
+            {
+                throw new ArgumentNullException("objectGenerator");
+            }
+
+            _objects = new ConcurrentBag<T>();
+            _objectGenerator = objectGenerator;
+        }
+
+        ~Pool()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Removes and returns an object from the <see cref="Pool{T}"/>.<br />
+        /// If the pool is empty, then create a new instance of the <typeparamref name="T"/>.
+        /// </summary>
+        /// <returns>An object.</returns>
+        public T Get()
+        {
+            T item;
+            if (_objects.TryTake(out item))
+            {
+                return item;
+            }
+            
+            return _objectGenerator();
+        }
+
+        /// <summary>
+        /// Puts an object into the pool.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Put(T item)
+        {
+            _objects.Add(item);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposeManaged)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (disposeManaged)
+            {
+                foreach (var item in _objects)
+                {
+                    var disposable = item as IDisposable;
+
+                    if (disposable != null)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+            }
+
+            _isDisposed = true;
+        }
+    }
+}
---
+++ Tizsoft.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tizsoft.Data.dll")]
+[assembly: AssemblyDescription("Tizsoft.Data.dll")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TIZSoft Digital Technology Co., Ltd.")]
+[assembly: AssemblyProduct("Tizsoft.Data")]
+[assembly: AssemblyCopyright("Copyright © 2014 TIZSoft Digital Technology Co., Ltd. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ab6960c3-82be-40f5-ac3a-eaae9f27e7be")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
