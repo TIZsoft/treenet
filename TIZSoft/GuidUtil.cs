93a410004e801f0a08782813a392d1ff3abeb9d4
Mars <mars@tizsoft.com>
2014-09-24 15:23:29 +0800

    - Add GuidUitl class

---
+++ TIZSoft/GuidUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tizsoft
+{
+    public static class GuidUtil
+    {
+        public static Guid New()
+        {
+            return Guid.NewGuid();
+        }
+
+        public static Guid Get(string guidValue)
+        {
+            return new Guid(guidValue);
+        }
+
+        public static string ToBase64(Guid guid)
+        {
+            return ToBase64(guid.ToByteArray());
+        }
+
+        public static string ToBase64(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static Guid FormBase64(string value)
+        {
+            var guidBytes = Convert.FromBase64String(value);
+            return new Guid(guidBytes);
+        }
+    }
+}
