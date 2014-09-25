d78f9e36a1d97daadce62a8be4f3f19a200b02e0
Mars <mars@tizsoft.com>
2014-09-25 16:06:14 +0800

    -Feature, add cache, cache data.

---
+++ TIZSoft/Caching/Cache/Cache.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Tizsoft.Caching.Cache
+{
+    public class Cache<T> where T: class, new()
+    {
+        readonly Dictionary<string, CacheData<T>> _cacheObjects;
+
+        public Cache()
+        {
+            _cacheObjects = new Dictionary<string, CacheData<T>>();
+        }
+
+        public void Add(string key, T obj)
+        {
+            var cacheObj = new CacheData<T>(obj);
+            _cacheObjects.Add(key, cacheObj);
+        }
+
+        public T Get(string key)
+        {
+            if (!_cacheObjects.ContainsKey(key))
+            {
+                return default(T);
+            }
+
+            _cacheObjects[key].UpdateCount();
+            return _cacheObjects[key].CachedObject;
+        }
+
+        public void Remove(string key)
+        {
+            if (!_cacheObjects.ContainsKey(key))
+            {
+                return;
+            }
+
+            _cacheObjects.Remove(key);
+        }
+    }
+}
---
+++ TIZSoft/Caching/CacheData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tizsoft.Caching
+{
+    class CacheData<T> where T : class
+    {
+        public T CachedObject { get; private set; }
+        public long Time { get; private set; }
+        public uint Count { get; private set; }
+
+        public CacheData(T obj)
+        {
+            CachedObject = obj;
+            Time = DateTime.UtcNow.Ticks;
+            Count = 0;
+        }
+
+        public void UpdateCount()
+        {
+            Count++;
+            Time = DateTime.UtcNow.Ticks;
+        }
+    }
+}
