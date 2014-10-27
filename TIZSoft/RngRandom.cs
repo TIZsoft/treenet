eac8678c123a615ddae80e494b5a30020a807bc9
Simon Lin <simonlin@tizsoft.com>
2014-10-27 16:48:17 +0800

    - add SimpleHttpRequest class. - add RngRandom random number generator.

---
+++ TIZSoft/RngRandom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Tizsoft
+{
+    public class RngRandom
+    {
+        static readonly RandomNumberGenerator _generator = new RNGCryptoServiceProvider();
+        static readonly byte[] _randomBytes = new byte[4];
+
+        /// <summary>
+        /// generate one non-negative integer
+        /// </summary>
+        public static int Next()
+        {
+            _generator.GetBytes(_randomBytes);
+            int value = BitConverter.ToInt32(_randomBytes, 0);
+            return value < 0 ? -value : value;
+        }
+
+        /// <summary>
+        /// generate one non-negative interger less than max(excluded)
+        /// </summary>
+        /// <param name="max">max value</param>
+        public static int Next(int max)
+        {
+            _generator.GetBytes(_randomBytes);
+            int value = BitConverter.ToInt32(_randomBytes, 0);
+            value %= max;
+            return value < 0 ? -value : value;
+        }
+
+        /// <summary>
+        /// generate one non-negative integer between min(included) and max(excluded)
+        /// </summary>
+        /// <param name="min">min value</param>
+        /// <param name="max">max value</param>
+        public static int Next(int min, int max)
+        {
+            return Next(max - min) + min;
+        }
+    }
+}
---
+++ TIZSoft/SimpleHttpRequest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+namespace SpeedRunning
+{
+    public class SimpleHttpRequest
+    {
+        public static void HttpPostRequest(string url, NameValueCollection valueCollection,
+            UploadValuesCompletedEventHandler uploadValuesCompletedEventHandler, object userToken = null)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                wc.UploadValuesCompleted += uploadValuesCompletedEventHandler;
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                wc.UploadValuesAsync(new Uri(url), "POST", valueCollection, userToken);
+            }
+        }
+
+        public static void HttpPostRequest(string url, byte[] data,
+            UploadDataCompletedEventHandler uploadDataCompletedEventHandler, object userToken = null)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                wc.UploadDataCompleted += uploadDataCompletedEventHandler;
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                wc.UploadDataAsync(new Uri(url), "POST", data, userToken);
+            }
+        }
+
+        public static void DownloadData(string url, DownloadDataCompletedEventHandler downloadDataCompletedEventHandler,
+            object userToken = null)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                wc.DownloadDataCompleted += downloadDataCompletedEventHandler;
+                wc.DownloadDataAsync(new Uri(url), userToken);
+            }
+        }
+    }
+}
