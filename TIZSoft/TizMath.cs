f16c17801e5ce78a2f92931d3129ec486445799c
Simon Lin <simonlin@tizsoft.com>
2014-10-30 18:03:39 +0800

    - add math utility.

---
+++ TIZSoft/TizMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tizsoft
+{
+    public class TizMath
+    {
+        /// <summary>
+        /// 真．四捨五入
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static double Round(double value)
+        {
+            return Math.Round(value, MidpointRounding.AwayFromZero);
+        }
+
+        /// <summary>
+        /// 真．四捨五入
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static decimal Round(decimal value)
+        {
+            return Math.Round(value, MidpointRounding.AwayFromZero);
+        }
+    }
+}
