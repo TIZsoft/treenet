c6ca88602a6fdf00a5bba2ea55f6c813e7d9231c
simon <simonlin@tizsoft.com>
2015-06-13 14:12:12 +0800

    - add two time extensions.

--- TIZSoft/Extensions/DateTimeExt.cs
+++ TIZSoft/Extensions/DateTimeExt.cs
@@ -9,5 +9,15 @@ namespace Tizsoft.Extensions
         {
             return dateTime.ToString(Utils.MysqlTimeStampFormat);
         }
+
+        public static long ToUnixTimeStamp(this DateTime dateTime)
+        {
+            return (long)(dateTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        public static long UtcNowToUtcTimeStamp(this DateTime dateTime)
+        {
+            return (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
     }
 }
