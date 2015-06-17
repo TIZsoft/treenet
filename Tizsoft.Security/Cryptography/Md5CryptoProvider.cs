8161df66e2e0e0214e74d95b0901ee9d59e15226
simon <simonlin@tizsoft.com>
2015-06-17 11:19:32 +0800

    - add md5 crypto provider.

---
+++ Tizsoft.Security/Cryptography/Md5CryptoProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Tizsoft.Security.Cryptography
+{
+    public class Md5CryptoProvider : ICryptoProvider
+    {
+        public byte[] Encrypt(byte[] plain)
+        {
+            if (plain == null || plain.Length == 0)
+                throw new ArgumentNullException();
+
+            using (var md5 = MD5.Create())
+                return md5.ComputeHash(plain);
+        }
+
+        public byte[] Encrypt(byte[] plain, int offset, int count)
+        {
+            var result = new List<byte>(Encrypt(plain));
+            return result.Skip(offset).Take(count).ToArray();
+        }
+
+        public byte[] Decrypt(byte[] cipher)
+        {
+            throw new InvalidOperationException("MD5 can't be decrypt");
+        }
+
+        public byte[] Decrypt(byte[] cipher, int offset, int count)
+        {
+            throw new InvalidOperationException("MD5 can't be decrypt");
+        }
+    }
+}
---
+++ Tizsoft.Security/Cryptography/Md5Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Tizsoft.Security.Cryptography
+{
+    public class Md5Utils
+    {
+        public static string ToHexString(byte[] contents)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var t in contents)
+                sb.Append(t.ToString("x2"));
+
+            return sb.ToString();
+        }
+
+        public static string ToHexString(byte[] contents, int from, int count)
+        {
+            return ToHexString(contents).Substring(from, count);
+        }
+    }
+}
